8578cf30d5a4b757259f833ed7e87f8f3dcc38ce
Domen Blenkuš <domen@blenkus.com>
2019-11-24 18:05:39 +0100

    Add Django REST Framework

--- rola/settings.py
+++ rola/settings.py
@@ -34,6 +34,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rola.user',
 ]
 
 MIDDLEWARE = [
@@ -108,6 +111,19 @@ if not config('ROLA_DISABLE_PASSWORD_VALIDATORS', default=False):
         {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
         {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
     ]
+
+# Django REST Framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
