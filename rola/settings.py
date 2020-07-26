8a0eb526d8935fbcd10ec77d1810dd418c60cd5c
Domen Blenkuš <domen@blenkus.com>
2020-07-26 18:43:21 +0200

    Add django filters configuration

--- rola/settings.py
+++ rola/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'rolca.core',
     'drf_user.apps.UserConfig',
     'storages',
@@ -137,6 +138,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ['drf_user.authentication.TokenAuthentication'],
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.backends.DjangoFilterBackend',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
